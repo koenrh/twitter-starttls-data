87238aad60d40145e0be8a59d4247e39205dea47
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-01-06 17:32:42 +0100

    Fix update script

--- update.sh
+++ update.sh
@@ -3,14 +3,15 @@
 mkdir -p tmp
 touch history.txt
 
-# strip first and last line to make it a valid JSON file
-curl https://ton.twitter.com/starttls/tls.json | tail -n +2 | head -n -1 > tmp/tls.json
-DATE=$(jq ".global.now" tmp/tls.json)
+# strip first line to make it a valid JSON file
+curl https://ton.twitter.com/starttls/tls.json | tail -n +2 > tmp/tls.json
+DATE=$(/usr/local/bin/jq ".global.now" tmp/tls.json)
+
+echo "$(date +%Y%m%d%H%M%S) $DATE"
 
 if ! grep -q "$DATE" history.txt; then
-  jq ".domains" tmp/tls.json | python -mjson.tool > domains.json
-  jq ".global" tmp/tls.json | python -mjson.tool > global.json
-  #jq ".domains" tls.json | jq -r "to_entries[] | .key | @text" | sort > domains.txt
+  /usr/local/bin/jq ".domains" tmp/tls.json | python -mjson.tool > domains.json
+  /usr/local/bin/jq ".global" tmp/tls.json | python -mjson.tool > global.json
   echo "$DATE" >> history.txt
   git add domains.json global.json history.txt
   git commit -m "Update TLS data $DATE"
